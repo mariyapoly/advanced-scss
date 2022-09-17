d3e743828d0e40f6855b68ba6551b26b7c653f8a
Ashraf Mollah <mollah.dev@gmail.com>
2022-09-17 23:27:57 +0600

    class 6 completed

---
+++ src/app.js
@@ -0,0 +1,1 @@
+import './classes/class-6';
---
+++ src/classes/class-6.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//premitive - reference
+// string -> "one world" 'string'
+// number -> "100" , 100.5
+// Boolean -> true false
+// BigInt -> 
+// Symbol
+// undefines 
+// null
+
+// stack 
+
+// reference
+//object array
+
+// heap 
