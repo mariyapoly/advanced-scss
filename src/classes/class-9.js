69a25e3bce102cc1791c4bb3025f536b24371172
Ashraf Mollah <mollah.dev@gmail.com>
2022-10-08 23:05:59 +0600

    getter , setter

---
+++ src/classes/class-9.js
@@ -0,0 +1,106 @@
+import databse from '../json/8-database.json';
+
+const enum_divider = {
+    start: 'begin_',
+    end: 'end_'
+}
+
+function isString( prop ) {
+    return typeof prop === 'string'
+}
+// start_header = header;
+// end_header = header;
+function findID( prop, isStart = true ) {
+    const type = isStart ? enum_divider.start : enum_divider.end;
+    const original_id = prop.split(type)[1];
+    return original_id  
+}
+// start_header = end_header
+// end_header = start_header
+function reverseDivider( prop, isStart ) {
+    const type        = isStart ? enum_divider.start : enum_divider.end;
+    const reverseType = isStart ? enum_divider.end : enum_divider.start;
+    const original_id = prop.split(type)[1];
+    return reverseType + original_id;
+}
+
+const isDivider = function ( prop ) {
+    let result = {
+        isStart : false,
+        isEnd   : false,
+    };
+    if( isString( prop ) && ( prop.startsWith( enum_divider.start ) || prop.startsWith( enum_divider.end ) ) ) {
+        const isStart   = prop.startsWith( enum_divider.start );
+        const id        = findID( prop, isStart );
+        const reverse   = reverseDivider( prop, isStart );
+        const type      = isStart ? 'isStart' : 'isEnd';
+        
+        if( Reflect.has( databse, reverse ) && !Reflect.has( databse, id ) ) {
+            result[type] = true;
+        }else {
+            console.warn('Something is wrong with ', prop)
+        }
+    }
+    return  result;
+}
+
+const queue = [];
+const data = {}
+
+
+for( let property in databse ) {
+    const { isStart, isEnd } =  isDivider( property );
+    let canUse      = true;
+
+    if( isStart ) {
+       const id = findID( property );
+       queue.push( id );
+       canUse = false;
+    }
+
+    if( isEnd ) {
+        const id = findID( property, false );
+        const ind = queue.indexOf( id );
+        queue.splice( ind, 1 );
+        canUse = false;
+    }
+
+    let endpoint = data;
+    for( let scope of queue ) {
+        if( !Reflect.has( endpoint, scope ) ) {
+            endpoint[scope] = {};
+        }
+        endpoint = endpoint[scope];
+    }
+
+    if( canUse ) {
+        endpoint[property] = databse[property];
+    }
+    
+}
+
+// console.log(data)
+
+
+// const d = new Date();
+// let year = d.getFullYear();
+
+// const student = {
+//     name: 'Sani',
+//     bid: 1990,
+//     email: 'test@gmail.com',
+//     location: 'Bangladesh',
+//     get age(){
+//         return year - this.bid
+//     },
+//     set brithday( newYear ) {
+//         if( newYear >= year ) {
+//             console.error('Wrong brithday');
+//         } else {
+//             this.bid = newYear
+//         }
+//     }
+// }
+
+// student.brithday = 2025
+// console.log(student.age)
